f62fa37aa7dac347020447ddca3559276edc1406
Samuel Lye <sl2982@cornell.edu>
2021-12-21 00:18:12 +0800

    Updates

---
+++ precache-manifest.5b2165e567fe735a9db1608a6cc3de03.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "b573d17733cb45252edc",
+    "url": "/portfolio/static/css/main.7a8647ad.chunk.css"
+  },
+  {
+    "revision": "b573d17733cb45252edc",
+    "url": "/portfolio/static/js/main.acaa008a.chunk.js"
+  },
+  {
+    "revision": "1f22d2a392bab1f34734",
+    "url": "/portfolio/static/js/runtime~main.c7450590.js"
+  },
+  {
+    "revision": "a0e92412c76fa1033b59",
+    "url": "/portfolio/static/js/2.dce24c9e.chunk.js"
+  },
+  {
+    "revision": "d6feae6ccdd65781be0c",
+    "url": "/portfolio/static/js/3.b601966e.chunk.js"
+  },
+  {
+    "revision": "eaf4913998b3b53941a385ea1f0f5a9b",
+    "url": "/portfolio/static/media/icomoon.eaf49139.ttf"
+  },
+  {
+    "revision": "75899192059ab3a3018405d89f6e27aa",
+    "url": "/portfolio/static/media/icomoon.75899192.eot"
+  },
+  {
+    "revision": "3409d201de73f21e3c4746e2942c273d",
+    "url": "/portfolio/static/media/icomoon.3409d201.woff"
+  },
+  {
+    "revision": "e7f6454a7d076819823f30bd1bb64217",
+    "url": "/portfolio/static/media/icomoon.e7f6454a.svg"
+  },
+  {
+    "revision": "218ad3cea8c88284cd34b0e9e0fec052",
+    "url": "/portfolio/static/media/logo.218ad3ce.png"
+  },
+  {
+    "revision": "2024662c171e9ffd2b43e04ec6f82208",
+    "url": "/portfolio/static/media/bloomberg.2024662c.png"
+  },
+  {
+    "revision": "4eb38eb5005f5507d42eca52fa570fb7",
+    "url": "/portfolio/static/media/govtech.4eb38eb5.gif"
+  },
+  {
+    "revision": "adc3cd3e91f855fa5a937f8aaa5479e1",
+    "url": "/portfolio/static/media/alice.adc3cd3e.png"
+  },
+  {
+    "revision": "fa38b2cd5cfa2b1e3caffbc138df878b",
+    "url": "/portfolio/static/media/opencerts.fa38b2cd.png"
+  },
+  {
+    "revision": "8fd03250624c274a06786931444d59e8",
+    "url": "/portfolio/static/media/workpass.8fd03250.png"
+  },
+  {
+    "revision": "8dcbbcc57af0a88ce5c6ea23d2948654",
+    "url": "/portfolio/static/media/minibot.8dcbbcc5.png"
+  },
+  {
+    "revision": "9b59c2ea4c597b759087e64e24d9c2c5",
+    "url": "/portfolio/static/media/culambdas.9b59c2ea.jpg"
+  },
+  {
+    "revision": "f77014e74acf844d20ab92eb7454482e",
+    "url": "/portfolio/static/media/secretsanta.f77014e7.png"
+  },
+  {
+    "revision": "99ce7d9bc5f9e571db1eab0e1a72049b",
+    "url": "/portfolio/static/media/ccup.99ce7d9b.png"
+  },
+  {
+    "revision": "536e2b2eec7a841208d8d6afc7720a1d",
+    "url": "/portfolio/static/media/corsell.536e2b2e.png"
+  },
+  {
+    "revision": "9aedda5c981e4ea1a48385b44f51ce73",
+    "url": "/portfolio/index.html"
+  }
+];
